bf060182a26aca37d53c02a7fe49881a85b60870
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-03 11:42:33 +0530

    made time dynamic

--- script2.js
+++ script2.js
@@ -7,7 +7,20 @@ let maxTemp=document.getElementById("maxTemperature");
 // let couGiven;
 cName.value=localStorage.getItem("cityD");
 couName.value=localStorage.getItem("countryD");
-// cName.value=cgiven;
-let date=new Date();
-let formatDate=date.toLocaleString(date);
-document.getElementById("sideHeading").innerHTML=formatDate;
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cName.value},In&APPID=ac731293d11478fe443d41ecac1c4b4b`)
+    .then((response)=>
+    {
+        return response.json;
+    })
+    .then((data)=>
+    {
+        let author=data;
+        console.log(author);
+    })
+
+setInterval(()=>
+{
+    let date=new Date();
+    let formatDate=date.toLocaleString(date);
+    document.getElementById("sideHeading").innerHTML=formatDate;
+},1000);
