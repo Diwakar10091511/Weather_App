3921e867b30a330116d8c2e9ab9d7cf2c7c01c34
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-03 12:56:05 +0530

    npm initialised and server created at local host 8000

--- script2.js
+++ script2.js
@@ -1,26 +1,48 @@
-let cName=document.getElementById("cityName");
-let couName=document.getElementById("countryName");
-let temp=document.getElementById("temperature");
-let minTemp=document.getElementById("minTemperature");
-let maxTemp=document.getElementById("maxTemperature");
+
+const http=require("http");
+const fs=require("fs");
+const requests=require("requests");
+
+const server=http.createServer((req,res)=>
+{
+    if(req.url=="/")
+    {
+        requests(
+            "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ac731293d11478fe443d41ecac1c4b4b")
+            
+        .on("data",(chunk)=>
+        {
+            let objData=JSON.parse(chunk);
+            console.log(objData);
+        })
+
+        .on("end",(err)=>
+        {
+            if(err)
+            console.log(err)
+        });
+    }
+});
+
+server.listen(8000,"127.0.0.1");
+
+
+
+
+
+// let cName=document.getElementById("cityName");
+// let couName=document.getElementById("countryName");
+// let temp=document.getElementById("temperature");
+// let minTemp=document.getElementById("minTemperature");
+// let maxTemp=document.getElementById("maxTemperature");
 // let cGiven;
 // let couGiven;
-cName.value=localStorage.getItem("cityD");
-couName.value=localStorage.getItem("countryD");
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cName.value},In&APPID=ac731293d11478fe443d41ecac1c4b4b`)
-    .then((response)=>
-    {
-        return response.json;
-    })
-    .then((data)=>
-    {
-        let author=data;
-        console.log(author);
-    })
+// cName.value=localStorage.getItem("cityD");
+// couName.value=localStorage.getItem("countryD");
 
-setInterval(()=>
-{
-    let date=new Date();
-    let formatDate=date.toLocaleString(date);
-    document.getElementById("sideHeading").innerHTML=formatDate;
-},1000);
+// setInterval(()=>
+// {
+//     let date=new Date();
+//     let formatDate=date.toLocaleString(date);
+//     document.getElementById("sideHeading").innerHTML=formatDate;
+// },1000);
