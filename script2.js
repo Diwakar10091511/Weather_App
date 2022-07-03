12ec661d9e1e820ddc82e45423302bc9e099b7ed
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-03 23:36:44 +0530

    I was trying to use http server and requests module to get api data but i am unable to use this data and put in the required fields of my html document. even if i put it , css is not getting linked.

--- script2.js
+++ script2.js
@@ -1,48 +1,58 @@
 
-const http=require("http");
-const fs=require("fs");
-const requests=require("requests");
+// const http=require("http");
+// const fs=require("fs");
+// const requests=require("requests");
 
-const server=http.createServer((req,res)=>
-{
-    if(req.url=="/")
-    {
-        requests(
-            "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ac731293d11478fe443d41ecac1c4b4b")
-            
-        .on("data",(chunk)=>
-        {
-            let objData=JSON.parse(chunk);
-            console.log(objData);
-        })
 
-        .on("end",(err)=>
-        {
-            if(err)
-            console.log(err)
-        });
-    }
-});
-
-server.listen(8000,"127.0.0.1");
+// // localStorage.setItem("cityD","satna");
+// // var city=localStorage.getItem("cityD","satna");
 
 
 
+// // const server=http.createServer((req,res)=>
+// // {
+// if(req.url=="/")
+// {
+//     requests(
+//         `https://api.openweathermap.org/data/2.5/weather?q=Satna,In&APPID=ac731293d11478fe443d41ecac1c4b4b`)
+        
+//     .on("data",(chunk)=>
+//     {
+//         let objData=JSON.parse(chunk);
+//         let arrData=[objData];
+//         console.log(`${Math.round((arrData[0].main.temp-273.00)*100)/100}`);
+//     })
+
+//     .on("end",(err)=>
+//     {
+//         if(err)
+//         console.log(err)
+//     });
+// }
+// //     }
+// //     let mainData=fs.readFileSync("index.html","utf-8");
+// //     // console.log(mainData);
+// //     res.write(mainData);
+// //     res.end();
+
+// // });
+
+// // server.listen(8000,"127.0.0.1");
 
 
-// let cName=document.getElementById("cityName");
-// let couName=document.getElementById("countryName");
+
+
+
+let cName=document.getElementById("cityName");
+let couName=document.getElementById("countryName");
 // let temp=document.getElementById("temperature");
 // let minTemp=document.getElementById("minTemperature");
 // let maxTemp=document.getElementById("maxTemperature");
-// let cGiven;
-// let couGiven;
-// cName.value=localStorage.getItem("cityD");
-// couName.value=localStorage.getItem("countryD");
-
-// setInterval(()=>
-// {
-//     let date=new Date();
-//     let formatDate=date.toLocaleString(date);
-//     document.getElementById("sideHeading").innerHTML=formatDate;
-// },1000);
+cName.value=localStorage.getItem("cityD");
+couName.value=localStorage.getItem("countryD");
+setInterval(()=>
+{
+    let date=new Date();
+    let formatDate=date.toLocaleString(date);
+    document.getElementById("sideHeading").innerHTML=formatDate;
+},1000);
