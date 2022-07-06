cd13e66e21bfbfb2059e2819a74e307dbc6ad496
Diwakar10091511 <diwakarnew1123@gmail.com>
2022-07-06 22:17:28 +0530

    alert added for empty city name

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function fetchDetails()
     // var cougiven=document.getElementById("countryDetail").value;
     localStorage.setItem("cityD",cgiven);
     // localStorage.setItem("countryD",cougiven);
-    if (cgiven=" ") {
+    if (cgiven=="") {
         alert("Please enter your city name");
     } else {
      location.href="index2.html";    
